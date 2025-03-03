878aa3f56c7a0f90d92900e9a2d895cc14defc54
Tiffany Zhang <tzhang@lcs.on.ca>
2025-03-03 15:07:43 -0500

    Improved view so that the author doesn't show up right away.

--- QuoteFinder/View/QuoteView.swift
+++ QuoteFinder/View/QuoteView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct QuoteView: View {
     
     @State var viewModel = QuoteViewModel()
+    
+    @State var authorOpacity = 0.0
+
+    @State var authorTimer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         VStack {
             if let currentQuote = viewModel.currentQuote{
@@ -17,6 +22,16 @@ struct QuoteView: View {
                     Text(currentQuote.quoteText ?? "")
                         .padding(.bottom, 100)
                     Text(currentQuote.quoteAuthor ?? "")
+                        .opacity(authorOpacity)
+                        .onReceive(authorTimer) { _ in
+                            
+                            withAnimation {
+                                authorOpacity = 1.0
+                            }
+                            
+                            // Stop the timer
+                           authorTimer.upstream.connect().cancel()
+                        }
                 }
                 .font(.title)
                 .multilineTextAlignment(.center)
