1ec8832dd6637033a7f371fb8610e78370b01738
Tiffany Zhang <tzhang@lcs.on.ca>
2025-03-03 14:43:43 -0500

    Initial Commit

---
+++ QuoteFinder/QuoteFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteFinderApp.swift
+//  QuoteFinder
+//
+//  Created by xinyu zhang on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct QuoteFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
