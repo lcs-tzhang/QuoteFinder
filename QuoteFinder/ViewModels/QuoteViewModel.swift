d5db4a1ca8dd7964120e0b26d82b1fc90b90ac4a
Tiffany Zhang <tzhang@lcs.on.ca>
2025-03-03 14:59:03 -0500

    Added a function to the view model to retrieve a joke from the view model.

--- QuoteFinder/ViewModels/QuoteViewModel.swift
+++ QuoteFinder/ViewModels/QuoteViewModel.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+
+
+@Observable
+class QuoteViewModel{
+    
+    //MARK: Stored properties
+    
+    var currentQuote: Quote?
+    
+    //MARK: Intializer(s)
+    init(currentQuote: Quote? = nil) {
+        self.currentQuote = currentQuote
+        
+        Task{
+            await self.fetchQuote()
+            
+        }
+    }
+    
+    //MARK: Function(s)
+    
+    func fetchQuote() async{
+        
+        let endpoint = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en"
+        guard let url = URL(string: endpoint) else{
+            print("Invalid address for JSON endpoint.")
+            return
+        }
+        
+        do{
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            
+            print("Got data from endpoint, contents of response are:")
+            print(String(data: data, encoding: .utf8)!)
+            
+            
+            let decoder = JSONDecoder()
+            
+            let decodedData = try decoder.decode(Quote.self, from: data)
+            
+            self.currentQuote = decodedData
+            
+        } catch{
+            
+            print("Count not retrieve data from endpoint, or could not decode into an instance of a Swift data type.")
+            print("----")
+            print(error)
+            
+        }
+    }
+}
